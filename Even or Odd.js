578c1d8e6faf23b75b7b66743b9f9238a39e0d95
katrin2010 <katrin-2010@hotmail.com>
2020-05-29 14:33:25 -0500

    Added solution of this task.

---
+++ Even or Odd.js
@@ -0,0 +1,10 @@
+//https://www.codewars.com/kata/53da3dbb4a5168369a0000fe/
+//Even or Odd
+
+function even_or_odd(number) {
+    if (number % 2)
+        return "Odd";
+    else
+        return "Even";
+}
+
